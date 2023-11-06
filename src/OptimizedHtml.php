0bc71d22054df86a8cd175ce3d4cde2fd0fdfbf5
nielsoffice <nieldigitalsolution@gmail.com>
2023-11-07 06:58:21 +0800

    init error handling

--- src/OptimizedHtml.php
+++ src/OptimizedHtml.php
@@ -1,6 +1,10 @@
 <?php
-
+ 
  namespace PHPWineOptimizedHtml;
+ 
+ ini_set('display_errors', 1);
+ ini_set('display_startup_errors', 1);
+ error_reporting(E_ALL);
 
  /**
  * @copyright (c) 2023 Optimized Html Cooked by nielsoffice
